9a48392ceadac488d2987c16755fbabdaed98776
Rianna Jitosho <rjitosho@stanford.edu>
2021-01-05 15:23:38 -0800

    Add discrete_jacobian, rollout, and stabilization

--- examples/double_pendulum_mc.jl
+++ examples/double_pendulum_mc.jl
@@ -86,13 +86,55 @@ function discrete_dynamics_MC(::Type{Q},
   
 end
 
-function discrete_jacobian_MC!(::Type{Q}, ∇f, model::DoublePendulumMC,
-  z::AbstractKnotPoint{T,N,M}) where {T,N,M,Q<:RobotDynamics.Explicit}
+function discrete_jacobian_MC(::Type{Q}, model::DoublePendulumMC,
+  z::AbstractKnotPoint{T,N,M′}) where {T,N,M′,Q<:RobotDynamics.Explicit}
 
-  x, λ = discrete_dynamics_MC(Q, model, z)
+  n, m = size(model)
+  x = state(z) 
+  u = control(z)
+  t = z.t 
+  dt = z.dt
 
-  #take diff wrt x, \lambda 
+  m1 = model.m1
+  m2 = model.m2
+  l1 = model.l1
+  l2 = model.l2
+  g = model.g
+  M = Diagonal([m1,m1,1.0/12.0*m1*l1^2,m2,m2,1.0/12.0*m2*l2^2])
 
+  # compute next state and lagrange multiplier
+  x⁺, λ = discrete_dynamics_MC(Q, model, z)
+
+  # constraint function
+  dc(x) = dynamics_con(model, x)
+
+  # implicit function
+  function f_imp(z)
+    # Unpack
+    q⁺ = z[1:6]
+    v⁺ = z[7:12]
+    q = z[13:18]
+    v = z[19:24]
+    u = z[25:26]
+    λ = z[27:end]
+    
+    J = ForwardDiff.jacobian(dc, q⁺)
+    F = [0; -m1*g; u[1]-u[2];0; -m2*g; u[2]] * dt
+    return [M*(v⁺-v) - (J'*λ + F); q⁺ - (q + v⁺*dt)]
+  end
+
+  # compute jacobians
+  x = state(z) 
+  u = control(z)
+  all_partials = ForwardDiff.jacobian(f_imp, [x⁺;x;u;λ])
+  ABC = -all_partials[:,1:12]\all_partials[:,13:end]
+  A = ABC[:, 1:n]
+  B = ABC[:, n .+ (1:m)]
+  C = ABC[:, n+m+1:end]
+
+  J = ForwardDiff.jacobian(dc, x⁺[1:6])
+  G = [J zeros(4,6)]
+  return A,B,C,G
 end
 
 function RobotDynamics.discrete_dynamics(::Type{Q}, model::DoublePendulumMC, 
@@ -107,10 +149,10 @@ function RobotDynamics.discrete_jacobian!(::Type{Q}, ∇f, model::DoublePendulum
 
 end
 
-function TrajectoryOptimization.dynamics_expansion!(Q, D::Vector{<:DynamicsExpansion}, model::AbstractModel,
-  # x, λ = discrete_dynamics_MC(Q, model, z)
-  # A,B,C,G = discrete_jacobian_MC(Q, model x,  λ)
-end
+# function TrajectoryOptimization.dynamics_expansion!(Q, D::Vector{<:DynamicsExpansion}, model::AbstractModel,
+#   # x, λ = discrete_dynamics_MC(Q, model, z)
+#   # A,B,C,G = discrete_jacobian_MC(Q, model x,  λ)
+# end
 
 # Specify the state and control dimensions
 RobotDynamics.state_dim(::DoublePendulumMC) = 12
@@ -122,15 +164,33 @@ n,m = size(model)
 
 # Generate random state and control vector
 x,u = rand(model)
-dt = 0.1
+dt = 0.01
 z = KnotPoint(x,u,dt)
 
-
 # Evaluate the discrete dynamics and Jacobian
-x′ = discrete_dynamics(Euler, model, z)
+x′ = discrete_dynamics(PassThrough, model, z)
 println(x)
 println(x′)
 
-# roll out test cases
+A,B,C,G = discrete_jacobian_MC(PassThrough, model, z)
+println(A)
+
+# rollout dynamics
+th1 = -pi/4
+th2 = -pi/4
+d1 = .5*model.l1*[cos(th1);sin(th1)]
+d2 = .5*model.l2*[cos(th2);sin(th2)]
+x0 = [d1; th1; 2*d1 + d2; th2; zeros(6)]
+N = 200
+Z = Traj(n, m, dt, N)
+RobotDynamics.rollout!(RK2, model, Z, x0)
+
+# plot
+using Plots
+X = states(Z)
+th1 = [X[t][3] for t = 1:N]
+th2 = [X[t][6] for t = 1:N]
+plot([th1 th2]*180/pi)
+
 # expansion test cases
 
---
+++ examples/double_pendulum_mc_stabilize.jl
@@ -0,0 +1,126 @@
+# NOTE: run double_pendulum_mc.jl first
+
+#goal 
+th1 = -pi/4
+th2 = -pi/4
+d1 = .5*model.l1*[cos(th1);sin(th1)]
+d2 = .5*model.l2*[cos(th2);sin(th2)]
+xf = [d1; th1; 2*d1 + d2; th2; zeros(6)]
+
+#costs
+Q = zeros(n,n)
+Q[3,3] = 1e-3
+Q[6,6] = 1e-3
+Q[9,9] = 1e-3
+Q[12,12] = 1e-3
+Qf = zeros(n,n)
+Qf[3,3] = 250
+Qf[6,6] = 250
+Qf[9,9] = 250
+Qf[12,12] = 250
+R = 1e-4*Matrix(I,m,m)
+
+function f(x,u,dt)
+    z = KnotPoint(x,u,dt)
+    return discrete_dynamics_MC(PassThrough, model, z)
+end
+
+function getABCG(x⁺,x,u,λ,dt)
+    z = KnotPoint(x,u,dt)
+    return discrete_jacobian_MC(PassThrough, model, z)
+end
+
+function backwardpass(X,Lam,U,F,Q,R,Qf,xf)
+    n, N = size(X)
+    m = size(U,1)
+    nc = 4
+    
+    S = zeros(n,n,N)
+    s = zeros(n,N)    
+    K = zeros(m,n,N-1)
+    l = zeros(m,N-1)
+    
+    S[:,:,N] = Qf
+    s[:,N] = Qf*(X[:,N] - xf)
+    v1 = 0.0
+    v2 = 0.0
+
+    mu = 0.0
+    k = N-1
+    
+    while k >= 1
+        q = Q*(X[:,k] - xf)
+        r = R*(U[:,k])
+        S⁺ = S[:,:,k+1]
+        s⁺ = s[:,k+1]
+        
+        A,B,C,G = F(X[:,k+1],X[:,k],U[:,k],Lam[:,k],dt)
+        
+        D = B - C/(G*C)*G*B
+        M11 = R + D'*S⁺*B
+        M12 = D'*S⁺*C
+        M21 = G*B
+        M22 = G*C
+
+        M = [M11 M12;M21 M22]
+        b = [D'*S⁺;G]*A
+
+        K_all = M\b
+        Ku = K_all[1:m,:]
+        Kλ = K_all[m+1:m+nc,:]
+        K[:,:,k] = Ku
+
+        l_all = M\[r + D'*s⁺; zeros(nc)]
+        lu = l_all[1:m,:]
+        lλ = l_all[m+1:m+nc,:]
+        l[:,k] = lu
+
+        Abar = A-B*Ku-C*Kλ
+        bbar = -B*lu - C*lλ
+        S[:,:,k] = Q + Ku'*R*Ku + Abar'*S⁺*Abar
+        s[:,k] = q - Ku'*r + Ku'*R*lu + Abar'*S⁺*bbar + Abar'*s⁺
+
+        k = k - 1;
+    end
+    return K, l, v1, v2
+end
+
+function stable_rollout(Ku,x0,u0,f,dt,tf)
+    N = convert(Int64,floor(tf/dt))
+    X = zeros(size(x0,1),N)
+    U = zeros(m,N-1)
+    Lam = zeros(4,N-1)
+    X[:,1] = x0
+    for k = 1:N-1
+        U[:,k] = u0-Ku*(X[:,k]-xf)
+        X[:,k+1], Lam[:,k] = f(X[:,k],U[:,k],dt)
+    end
+    return X, Lam, U
+end
+
+#simulation
+dt = 0.01
+tf = 6.0
+
+# compute and verify nominal torques
+m1, m2, g = model.m1, model.m2, model.g
+uf = [(m1*xf[1] + m2*xf[4])*g; (xf[4] - 2*xf[1])*m2*g]
+xf′, λf = f(xf,uf,dt) # check xf′ = xf
+
+# compute stabilizing gains
+timesteps = 300
+X = repeat(xf,outer=(1,timesteps+1))
+Lam = repeat(λf,outer=(1,timesteps))
+U = repeat(uf,outer=(1,timesteps))
+K, l, v1, v2 = backwardpass(X,Lam,U,getABCG,Q,R,Q,xf)
+K6 = [K[1,6,i] for i=1:timesteps]
+K3 = [K[1,3,i] for i=1:timesteps]
+plot([K3 K6])
+
+# run stablizing controller
+Ku = K[:,:,1]
+x1, _ = f(xf,[1., 0],dt) # perturbance
+X, Lam, U=stable_rollout(Ku,x1,U[:,1],f,dt,tf)
+plot(X[3,:])
+plot!(X[6,:])
+println(norm(X[:,end]-xf))
