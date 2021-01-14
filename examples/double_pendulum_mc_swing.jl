9e7bd992a9ed7199a23d8c28eb8972cfe026fbcb
Shuo <paulyang1990@gmail.com>
2021-01-13 22:52:19 -0500

    test different swing states

--- examples/double_pendulum_mc_swing.jl
+++ examples/double_pendulum_mc_swing.jl
@@ -6,6 +6,10 @@ using TrajectoryOptimization
 using StaticArrays
 using LinearAlgebra
 using ForwardDiff
+# plot
+using Plots
+# visualize the system 
+using MeshCat
 
 const TO = TrajectoryOptimization
 const RD = RobotDynamics
@@ -19,18 +23,22 @@ dt = .01                  # number of knot points
 tf = (N-1)*dt           # final time
 
 # initial and final conditions
-th1 = -pi/4
-th2 = -pi/4
+th1 = -pi/2
+th2 = -pi/2
 d1 = .5*model.l1*[cos(th1);sin(th1)]
 d2 = .5*model.l2*[cos(th2);sin(th2)]
-x0 = [0; -.5*model.l1; -pi/2; 0; -model.l1-.5*model.l2; -pi/2; zeros(6)]
+x0 = [d1; th1; 2*d1 + d2; th2; zeros(6)]
+th1 = -pi/4
+th2 = pi/4
+d1 = .5*model.l1*[cos(th1);sin(th1)]
+d2 = .5*model.l2*[cos(th2);sin(th2)]
 xf = [d1; th1; 2*d1 + d2; th2; zeros(6)]
 
 # objective
 Q = zeros(n)
 Q[3] = Q[6] = Q[9] = Q[12] = 1e-3/dt
 Q = Diagonal(SVector{n}(Q))
-R = Diagonal(@SVector fill(1e-8/dt, m))
+R = Diagonal(@SVector fill(1e-4/dt, m))
 Qf = zeros(n)
 Qf[3] = Qf[6] = Qf[9] = Qf[12] = 2500
 Qf = Diagonal(SVector{n}(Qf))
@@ -49,12 +57,12 @@ rollout!(prob);
 
 
 # solve problem
-opts = SolverOptions(verbose=7)
+opts = SolverOptions(verbose=7,static_bp=0)
 solver = iLQRSolver(prob, opts);
 solve!(solver);
 
-# plot
-using Plots
 X2 = states(solver.Z)
-plot([X2[i][3] for i=1:N])
-plot!([X2[i][6] for i=1:N])
+gr(size = (2750, 2565))
+plot([X2[i][3] for i=1:N], linewidth=4,xtickfontsize=38,ytickfontsize=38,legendfontsize=38, label = ["joint 1 angle"],xlabel="time steps",xguidefontsize=38,ylabel="angle (rad)",yguidefontsize=38)
+plot!([X2[i][6] for i=1:N], linewidth=4,xtickfontsize=38,ytickfontsize=38,legendfontsize=38, label = ["joint 2 angle"],xlabel="time steps",xguidefontsize=38,ylabel="angle (rad)",yguidefontsize=38)
+
